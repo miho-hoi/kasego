efc73460ca13877996d850c3b0c8c7e218eeaa04
miho-hoi <miho0829ko@gmail.com>
2023-11-16 15:08:04 +0900

    各種微調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_13_213600) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_15_055804) do
+  create_table "payments", charset: "utf8", force: :cascade do |t|
+    t.date "date", null: false
+    t.integer "total_price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_payments_on_user_id"
+  end
+
   create_table "records", charset: "utf8mb4", force: :cascade do |t|
     t.date "date", null: false
     t.integer "times", null: false
@@ -42,4 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_13_213600) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "payments", "users"
 end
