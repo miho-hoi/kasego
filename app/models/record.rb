999e1235263f167688b4729b021e94d28f3e3b76
miho-hoi <miho0829ko@gmail.com>
2023-11-16 15:26:10 +0900

    バリデーション変更に伴うテストの再実行

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,5 +4,12 @@ class Record < ApplicationRecord
 
   validates :date,  presence: true
   validates :times, presence: true
-  
+
+  validate :date_should_be_today
+
+  private
+
+  def date_should_be_today
+    errors.add(:date, "は当日以外は登録できません") unless date == Date.today
+  end
 end
--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :record do
-    date  {Faker::Date.between(from: 2.days.ago, to: Date.today)}
+    date { Faker::Date.between(from: Date.today, to: Date.today) }
     times {Faker::Number.between(from: 1, to: 10)}
     association :user
     association :to_do
