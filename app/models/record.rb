b8b1cc4ceca5f3c05013b1494d3dce00ab98a801
miho-hoi <miho0829ko@gmail.com>
2023-11-16 15:03:15 +0900

    やったことを当日しかできないように変更

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,14 +4,5 @@ class Record < ApplicationRecord
 
   validates :date,  presence: true
   validates :times, presence: true
-
-  validate :date_cannot_be_in_the_future
   
-  private
-
-  def date_cannot_be_in_the_future
-    if date.present? && date > Date.today
-      errors.add(:date, "は今日以前の日付を選択してください")
-    end
-  end
 end
